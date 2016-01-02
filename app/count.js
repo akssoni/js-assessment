0ca5c8d1a5e177609c1b6cbfeee2cdc146f4839a
Akshay Soni <akshay.tech@hotmail.com>
2016-01-02 14:16:06 -0600

    Counter - done with all test cases

--- app/count.js
+++ app/count.js
@@ -1,8 +1,25 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function () {
-  return {
-    count : function (start, end) {
-    }
-  };
+	return {
+		count : function (start, end) {
+			var timeCancel;	                     
+
+			function myLoop () {    
+				console.log(start++); 
+				if (start <= end) { 
+					timeCancel = setTimeout(function () { myLoop();}, 100)
+				}
+			}
+
+			myLoop();   
+			
+			return {
+				cancel : function () {
+					clearTimeout(timeCancel);
+				}
+			};
+			
+		}
+	};
 });
