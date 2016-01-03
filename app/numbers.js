27f5b40c496dec487f656ad6e2227313d2f74943
Akshay Soni <akshay.tech@hotmail.com>
2016-01-02 19:23:05 -0600

    Logical Operators - Done all test cases Modules  - Done all test cases Numbers - Done all test cases

--- app/numbers.js
+++ app/numbers.js
@@ -4,18 +4,21 @@ define(function() {
   return {
     valueAtBit: function(num, bit) {
 
+        return 1 & num >> (bit - 1);
     },
 
     base10: function(str) {
-
+        return parseInt(str,2);
     },
 
     convertToBinary: function(num) {
-
+        var n = num.toString(2);
+        n = '00000000'.substr(n.length)+n;
+        return n;
     },
 
     multiply: function(a, b) {
-
+        return +(a*b).toFixed(8);
     }
   };
 });
