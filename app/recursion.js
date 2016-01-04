14ff0ddd2ab62790f17540159bc8678503cb2f08
Akshay Soni <akshay.tech@hotmail.com>
2016-01-03 23:19:10 -0600

    Permutation - All test cases done Functions - Pending - Curry test case

--- app/recursion.js
+++ app/recursion.js
@@ -51,7 +51,14 @@ define(function() {
 
 		},
 
-		permute: function(arr) {
+		permute: function(inputArray) {
+
+
+			var result = inputArray.reduce(function permute(res, item, key, arr) {
+				return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1)).reduce(permute, []).map(function(perm) { return [item].concat(perm); }) || item);
+			}, []);
+
+			return result;
 
 		}
 	};
